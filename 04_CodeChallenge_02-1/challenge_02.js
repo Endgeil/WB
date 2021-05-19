d11252fc2e328a7ee3878417c07f00cb9992b0c4
Endgeil <lexiwitte@yahoo.es>
2021-05-19 14:15:41 +0200

    lösung_02

--- 04_CodeChallenge_02-1/challenge_02.js
+++ 04_CodeChallenge_02-1/challenge_02.js
@@ -10,8 +10,49 @@ folgende Getränkeauswahl in die Konsole aus aus :
 Alter 0-5 // Milch  --> age > 0 AND age < 6
 Alter 6-12 // Saft
 Alter 13-17 // Cola
-18 + // Wein
+18 + // Bier
 
 */
 
 /* Getränke-Challenge */
+
+let eingabeAge,eingabeName;
+eingabeName = prompt("Bitte geben Sie Ihren Namen ein");
+eingabeAge = prompt("Bitte geben Sie Ihr Alter ein");
+
+/*
+switch (true) {
+    case (eingabeAge < 6):
+        console.log(eingabeName + " bekommt Milch!");
+        break;
+    case (eingabeAge < 13):
+        console.log(eingabeName + " bekommt Saft!");
+        break;   
+    case (eingabeAge < 18):
+        console.log(eingabeName + " bekommt Cola!");
+        break;
+    default:
+        console.log(eingabeName + " bekommt Bier!");
+        break;
+}
+*/
+
+
+if ((eingabeAge > 0) && (eingabeAge <6)) 
+{
+    console.log(eingabeName + " bekommt Milch!");
+}  
+else if ((eingabeAge > 6) && (eingabeAge < 13)) 
+{
+    console.log(eingabeName + " bekommt Saft!");
+}    
+else if ((eingabeAge > 12) && (eingabeAge < 18)) 
+{
+    console.log(eingabeName + " bekommt Cola!");
+}    
+else {
+    console.log(eingabeName + " bekommt Bier!");
+}
+
+
+
